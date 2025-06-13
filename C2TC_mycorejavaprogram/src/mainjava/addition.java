236251eebfdfcf08fda1b8e8c9f5c26a5fb4d64e
estherevanjalin <estherevanjalin04@gmail.com>
2025-06-13 21:28:54 +0530

    this is my java program

---
+++ C2TC_mycorejavaprogram/src/mainjava/addition.java
@@ -0,0 +1,15 @@
+package mainjava;
+import java.util.Scanner;
+
+public class addition {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a:");
+		int a = sc.nextInt();
+		System.out.println("enter b:");
+		int b = sc.nextInt();
+		int c=a+b;
+		System.out.println("the addition of c is:"+c);
+		}
+
+}
