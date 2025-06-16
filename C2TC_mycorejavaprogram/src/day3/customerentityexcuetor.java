f8c4752fecf94fc1550ad570577390d598e9c7f0
estherevanjalin <estherevanjalin04@gmail.com>
2025-06-16 13:51:30 +0530

    this is my java entity program

---
+++ C2TC_mycorejavaprogram/src/day3/customerentityexcuetor.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class customerentityexcuetor {
+	public static void main(String args[]) {
+		customerentity ct = new customerentity();
+		ct.setId(101);
+		ct.setName("esther");
+		ct.setCity("chennai");
+		System.out.println(ct);
+		customerentity ct1 =new customerentity();
+		ct1.setId(201);
+		ct1.setName("sam");
+		ct1.setCity("madurai");
+		System.out.println(ct1);
+		
+	}
+
+}
